e4eb347060ef71efeb6a1f06ba741a0f75c696a5
Aleksei Zhuikov <design.azv@gmail.com>
2023-02-27 22:17:11 +0300

    added favorites

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,10 +2,14 @@ import s from './index.module.css';
 import cn from 'classnames';
 import { useContext } from 'react';
 import { UserContext } from '../../context/userContext';
+import { Link } from 'react-router-dom';
+import { CardContext } from '../../context/cardContext';
+import { ReactComponent as FavoriteIcon } from './img/favorites.svg';
 
 function Header({ children, onUpdateUser }) {
 
   const { user: currentUser } = useContext(UserContext);
+  const { favorites } = useContext(CardContext);
   return (
     <header className={cn(s.header, 'cover')}>
       <div className="container">
@@ -15,6 +19,13 @@ function Header({ children, onUpdateUser }) {
           <div className={cn(s.userInfoContainer)}>
             {currentUser?.email && <span>{currentUser?.email}</span>}
             {currentUser?.name && <span>{currentUser?.name}: {currentUser?.about}</span>}
+          </div>
+
+          <div className={s.iconsMenu}>
+            <Link className={s.favoritesLink} to={{ pathname: "/favorites", state: 'sfsdfsdf' }}>
+              <FavoriteIcon />
+              {favorites.length !== 0 && <span className={s.iconBubble}>{favorites.length}</span>}
+            </Link>
           </div>
 
         </div>
